d0c5adf0a4d4c72065b07a23478e23312451569f
unknown <Gerogen@Gerogen.fit.by>
2016-12-25 19:13:56 +0300

    END VERSION

---
+++ GSO-2016/stdafx.h
@@ -0,0 +1,16 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <locale>
+#include <cwchar>
+#include <string>
+#include <ctime>
+#include <vector>
+#include <stack>
+
+
+using namespace std;
